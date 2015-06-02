ad88a748354f1242b41e780f385b1580ddce2492
rahul0090392 <rahul0090392@gmail.com>
2015-06-02 23:39:41 +0530

    Update Ex_1.12_word_per_line.c
    removed unnecessary function calls and now more concise.

--- languages/cprogs/Ex_1.12_word_per_line.c
+++ languages/cprogs/Ex_1.12_word_per_line.c
@@ -3,14 +3,13 @@
 
 int main() {
     int c;
-    c = getchar();
-    while (c != EOF) {
+    while ((c=getchar()) != EOF) {
         if (c == ' ') {
             putchar('\n');
         }
         else {
             putchar(c);
         }
-        c = getchar();
+        
     }
 }
