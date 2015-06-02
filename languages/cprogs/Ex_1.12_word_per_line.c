ad89a76ed4a654ec91bbdc8cda05e8249095b478
rahul0090392 <rahul0090392@gmail.com>
2015-06-02 23:35:40 +0530

    Update Ex_1.12_word_per_line.c
    More concise form.

--- languages/cprogs/Ex_1.12_word_per_line.c
+++ languages/cprogs/Ex_1.12_word_per_line.c
@@ -1,16 +1,20 @@
 /*Write a program that prints its input one word per line.*/
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-    int c;
-    c = getchar();
-    while (c != EOF) {
-        if (c == ' ') {
-            putchar('\n');
-        }
-        else {
-            putchar(c);
-        }
-        c = getchar();
-    }
+void main()
+{
+	int c;
+
+	while((c=getchar())!= EOF)
+	{
+		if(c == ' '){putchar('\n');}
+		else
+		{
+			putchar(c);
+			}
+			
+
+	}
+	
+	
 }
